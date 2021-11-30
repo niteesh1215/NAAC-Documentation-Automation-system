3e98462aaa0a46ec4e9758f991c9395444cc1678
Jerin Joji <jerinjoji1998@gmail.com>
2021-11-30 20:26:56 +0530

    modal closes automatically

--- src/app/components/root/main/files/files-header/files-header.component.ts
+++ src/app/components/root/main/files/files-header/files-header.component.ts
@@ -5,6 +5,7 @@ import { FormBuilder, Validators } from '@angular/forms'
 import { FilesApiService } from 'src/app/services/api_end_points/files_api.service';
 import { LResponse } from 'src/app/models/l_response'
 import { Router } from '@angular/router';
+import { NotifierService } from 'angular-notifier';
 
 @Component({
   selector: 'app-files-header',
@@ -28,7 +29,7 @@ export class FilesHeaderComponent implements OnInit {
     description: ['']
   });
 
-  constructor(private modalService: NgbModal, private formBuilder: FormBuilder, private _filesApiService: FilesApiService, private router: Router) { }
+  constructor(private modalService: NgbModal, private formBuilder: FormBuilder, private _filesApiService: FilesApiService, private router: Router, private notifierService: NotifierService) { }
 
   ngOnInit(): void {
     console.log(this.router.url)
@@ -39,7 +40,7 @@ export class FilesHeaderComponent implements OnInit {
     const formData = this.folderDetailsForm.value;
     this.file.name = formData.folderName;
     this.file.path = this.router.url;
-    this.file.description = formData.description;
+    this.file.description = formData.description.trim().length == 0 ? 'No Description' : formData.description 
     this.file.createdOn = Date.now();
 
     this._filesApiService.createFile({
@@ -52,10 +53,16 @@ export class FilesHeaderComponent implements OnInit {
       next: (lResponse: LResponse) => {
 
         console.log(lResponse);
-        
-
+        if(lResponse.status == 'success'){
+          this.notifierService.notify('success', 'Folder Created Successfully');
+          this.modalService.dismissAll();
+        }
+        else{
+          this.notifierService.notify('Failed', 'Some Error Occured');
+        }
+      
       }, error: (e) => {
-
+        this.notifierService.notify('Failed', 'Some Error Occured');
       }
     });
 
@@ -63,7 +70,9 @@ export class FilesHeaderComponent implements OnInit {
 
   openModal(content: any) {
     this.modalService.open(content);
+
   }
+
 
   changeView() {
     this.isGridView = !this.isGridView;
